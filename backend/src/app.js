ed244c2b9455e671cde0a4fc81ea84d47d243a36
imri.kalabrino@@@@@ <Imrikalabrino@gmail.com>
2024-10-28 22:45:38 +0200

    Added OAuth2 routes: redirect to GitLab and exchange authorization code for access token
    Signed-off-by: imri.kalabrino@@@@@ <Imrikalabrino@gmail.com>

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,35 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
+
+app.get('/auth/gitlab', (req, res) => {
+    const gitlabAuthUrl = `https://gitlab.com/oauth/authorize?client_id=${process.env.GITLAB_CLIENT_ID}&redirect_uri=${process.env.GITLAB_REDIRECT_URI}&response_type=code&scope=read_api`;
+    res.redirect(gitlabAuthUrl);
+});
+
+app.get('/auth/gitlab/callback', async (req, res) => {
+    const { code } = req.query;
+
+    try {
+        const tokenResponse = await axios.post('https://gitlab.com/oauth/token', {
+            client_id: process.env.GITLAB_CLIENT_ID,
+            client_secret: process.env.GITLAB_CLIENT_SECRET,
+            redirect_uri: process.env.GITLAB_REDIRECT_URI,
+            grant_type: 'authorization_code',
+            code: code,
+        });
+
+        const { access_token } = tokenResponse.data;
+
+        res.json({ access_token });
+    } catch (error) {
+        console.error('Error exchanging code for token:', error);
+        res.status(500).send('Authentication failed');
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('GitLab OAuth2 Integration Server');

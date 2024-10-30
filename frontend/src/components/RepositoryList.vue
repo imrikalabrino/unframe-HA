ce8142dbc54820a92f90c3f6da06ee430d18aed2
imri.kalabrino@@@@@ <Imrikalabrino@gmail.com>
2024-10-30 21:48:42 +0200

    Added UI Pagination support

--- frontend/src/components/RepositoryList.vue
+++ frontend/src/components/RepositoryList.vue
@@ -11,6 +11,11 @@
         <p>{{ repo.description }}</p>
       </li>
     </ul>
+
+    <div v-if="repositories.length">
+      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+      <button @click="nextPage">Next</button>
+    </div>
   </div>
 </template>
 
@@ -23,20 +28,37 @@ export default {
       repositories: [],
       loading: false,
       error: null,
+      currentPage: 1,
+      limit: 10,
     };
   },
   async mounted() {
-    this.loading = true;
-    try {
-      const response = await axios.get('http://localhost:3000/repositories?limit=10&page=1', {
-        withCredentials: true,
-      });
-      this.repositories = response.data;
-    } catch (err) {
-      this.error = 'Failed to fetch repositories';
-      console.error(err);
-    } finally {
-      this.loading = false;
+    await this.fetchRepositories();
+  },
+  methods: {
+    async fetchRepositories() {
+      this.loading = true;
+      try {
+        const response = await axios.get(`http://localhost:3000/repositories?limit=${this.limit}&page=${this.currentPage}`, {
+          withCredentials: true,
+        });
+        this.repositories = response.data;
+      } catch (err) {
+        this.error = 'Failed to fetch repositories';
+        console.error(err);
+      } finally {
+        this.loading = false;
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+        this.fetchRepositories();
+      }
+    },
+    nextPage() {
+      this.currentPage++;
+      this.fetchRepositories();
     }
   },
 };
